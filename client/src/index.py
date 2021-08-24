c0652e393278ee06dda0e272a9ab70e39c862a6a
Bjorn <saras.pp123@gmail.com>
2021-08-24 23:01:14 +0100

    Implementation first start

---
+++ client/src/index.py
@@ -0,0 +1,23 @@
+tblTodo= ["Clean bed", "Change computer parts", "Develop a functioning potateo"]
+choice = input("Please chooce what action would perform")
+
+def chooseTodo(tblTodo):
+  x = input("Please select what Todo ")
+  for x in tblTodo:
+    print(x)
+      
+def createTodo(tblTodo):
+  print(tblTodo)
+
+  
+
+
+def updateTodo(tblTodo):
+  print("Update")
+
+
+def deleteTodo(tblTodo):
+  print("Delete")
+
+
+
