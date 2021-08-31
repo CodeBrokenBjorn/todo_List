ff629de82ff1c14e52cac22d09365d443ae89b3d
Bjorn <saras.pp123@gmail.com>
2021-08-31 20:29:16 +0100

    I have more code into Todo need to change certain things

--- client/src/index.py
+++ client/src/index.py
@@ -1,23 +1,38 @@
-tblTodo= ["Clean bed", "Change computer parts", "Develop a functioning potateo"]
-choice = input("Please chooce what action would perform")
 
-def chooseTodo(tblTodo):
-  x = input("Please select what Todo ")
-  for x in tblTodo:
-    print(x)
-      
+tblTodo= ["Clean bed", "Change computer parts", "Develop a functioning potateo"]
+
+
+
+  
 def createTodo(tblTodo):
   print(tblTodo)
 
-  
-
 
 def updateTodo(tblTodo):
-  print("Update")
+  #So what I need to do is create if statment that will basiclly show that vallue is true
+  #You can do it in diffrent ways bool and ect...
+
+def deleteTodo(removeVal):
+  tblTodo.remove(removeVal)
+  print(tblTodo)
+
+def exitTodo(tblTodo):
+  exit()
+
+def chooseTodo():
+  tempChoose = (createTodo, updateTodo, deleteTodo)
+  x = input("Please select what Todo action you would like to have? ")
+  for x in tblTodo:
+    print(x)
+    removeVal = x
+    #Todo need to cadd way of adding or accessing other functions...
+  
+  
+    
 
 
-def deleteTodo(tblTodo):
-  print("Delete")
+      
 
 
 
+chooseTodo()
