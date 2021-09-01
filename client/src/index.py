6e643bbfe043ff824a6cb40aa23a5d7254c857e3
Bjorn <saras.pp123@gmail.com>
2021-09-01 19:18:45 +0100

    Menu fixed scrapped some useless code
    I have change certain things within the menu and cleared unwanted code.

--- client/src/index.py
+++ client/src/index.py
@@ -1,38 +1,58 @@
 
 tblTodo= ["Clean bed", "Change computer parts", "Develop a functioning potateo"]
 
-
-
+def menu():
+  print("[1] Create List")
+  print("[2] Update List")
+  print("[3] Delete List")
+  print("[4] Exit The program")
+  chooseTodo()
   
-def createTodo(tblTodo):
+def createTodo():
   print(tblTodo)
 
 
-def updateTodo(tblTodo):
+def updateTodo():
+  print("update")
   #So what I need to do is create if statment that will basiclly show that vallue is true
   #You can do it in diffrent ways bool and ect...
 
-def deleteTodo(removeVal):
-  tblTodo.remove(removeVal)
-  print(tblTodo)
+def deleteTodo():
+  print("Print delete")
+ # tblTodo.remove()
+  #print(tblTodo)
 
-def exitTodo(tblTodo):
+def exitTodo():
+  
   exit()
 
 def chooseTodo():
-  tempChoose = (createTodo, updateTodo, deleteTodo)
-  x = input("Please select what Todo action you would like to have? ")
-  for x in tblTodo:
-    print(x)
-    removeVal = x
-    #Todo need to cadd way of adding or accessing other functions...
-  
-  
+  #choiceX = input("Please 1select what Todo action y1ou would like to have? ") 
+ 
+  x = int(input("Please select what action you would like to do "))
+  while x != 0:
+    if x == 1:
+      createTodo();
+    elif x == 2:
+      updateTodo()
+    elif x == 3:
+      deleteTodo()
+    elif x == 4:
+      exitTodo()
+    else:
+      print("Error please input correct value and choose a following option")
+
+ 
+ 
+          
+
     
+  
 
+    
+    
+    
+    #I can add way to implement in for loop.
+    #Todo need to cadd way of adding or accessing other functions...
 
-      
-
-
-
-chooseTodo()
+menu()
