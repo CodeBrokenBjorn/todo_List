b7aeb3e186d3e1d44e4d7aa89cadf6be0a631533
Bjorn <saras.pp123@gmail.com>
2021-09-02 00:32:21 +0100

    Added function to create

--- client/src/index.py
+++ client/src/index.py
@@ -8,11 +8,38 @@ def menu():
   print("[4] Exit The program")
   chooseTodo()
   
+#Completed
 def createTodo():
+  numAmmountList = 0
+  for y in tblTodo:
+    numAmmountList+=1
+    print("(",numAmmountList,")",("{") , y ,("}"))
+  createInput = input("Input value: ")
+  tblTodo.append(createInput)
   print(tblTodo)
+  menu()
 
 
 def updateTodo():
+  numAmmountList = 0
+  for y in tblTodo:
+    numAmmountList+=1
+    print("(",numAmmountList,")",("{") , y ,("}"))
+#Ducking Fix this I cam't fox ot tored and ducking fed up....
+  choiceSelected = int(input("Please select what you want to update! "))
+  if tblTodo[y] == choiceSelected:
+    print("Choices selected do want to change it (Yes or No)")
+    inp= input().lower()
+    if inp == "yes":
+      tempChange = input("Please input what you wanted to change")
+      tblTodo[y] = tempChange
+      print("Example")
+    elif inp == "no":
+      updateTodo()
+
+
+        
+  
   print("update")
   #So what I need to do is create if statment that will basiclly show that vallue is true
   #You can do it in diffrent ways bool and ect...
@@ -29,22 +56,28 @@ def exitTodo():
 def chooseTodo():
   #choiceX = input("Please 1select what Todo action y1ou would like to have? ") 
  
-  x = int(input("Please select what action you would like to do "))
+  x = input("Please select what action you would like to do ")
   while x != 0:
-    if x == 1:
+    if x == "1":
       createTodo();
-    elif x == 2:
+      break
+    elif x == "2":
       updateTodo()
-    elif x == 3:
+      break
+    elif x == "3":
       deleteTodo()
-    elif x == 4:
+      break
+    elif x == "4":
       exitTodo()
     else:
       print("Error please input correct value and choose a following option")
+      
+      
 
  
  
           
+
 
     
   
